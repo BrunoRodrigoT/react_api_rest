a35fd54663cb11d5ce842e4cccda5d1951dfaac1
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-10-30 17:21:05 -0300

    atividade api github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import api from './services/api';
+
 
 function App() {
+
+  const [user, setUser] = useState('')
+  const [dados, setDados] = useState('')
+
+  function atribuirUser(e) {
+    e.preventDefault()
+    setUser(e.target.value)
+  }
+
+  function consultUser(){
+    let url = user
+    api.get(url).then(
+      (response) => {
+        if(!response.data.erro){
+          let objeto = JSON.stringify(response.data)
+          setDados(objeto)
+        }else{
+          setDados('Dados não encontrados')
+        }
+      }
+      ).catch(
+      (err) => {
+        setDados('Usuário não encontrado')
+      }
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+      <div>
+        <h1>Consultar usuário no Github</h1>
+        <input type='email' placeholder='Usuário' onChange={(e) => atribuirUser(e)}></input>
+        <button onClick={() => consultUser()}>Buscar</button>
+      </div>
+      <div>
+        {dados}
+      </div>
+    </>
   );
 }
 
